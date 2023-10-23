9f06b10f295f4395c099b7c8f74bae30f5ebbf5e
TshewangR <tshewang016@gmail.com>
2023-10-23 23:47:46 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,64 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Tshewang Rigzin",
+  "role": "Web Designer, Student",
+  "linkedinId":"- - - - - - - - - - - ",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I'm a computer science student with a passion for exploring the diverse facets of frontend development, whether it's diving into new technologies, crafting technical articles, or unwinding with a game in my spare moments.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I am currently studying under the programme B. Tech. Computer Science and Engineering with specialization in Cyber Physical Systems in Vellore Institute of Technology.",
+  "address":"India",
+  "website":"https://www.codeclassifiers.com",
+  "education":[
+    {
+      "UniversityName": "Vellore Institue of Technology",
+      "specialization":"CSE Cyber Physical Systems",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2025",
+      "Achievements":"AWS Certified Cloud Practitioner"
+    },
+
+  ],
+
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+ 
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
